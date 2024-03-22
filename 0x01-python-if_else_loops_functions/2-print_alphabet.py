30cc9ddff5f4a0ee82c63992318065fe56511486
En1max <moumne2255@gamil.com>
2024-03-21 23:03:30 -0700

    lowercase alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#program that prints the ASCII alphabet, in lowercase, not followed by a new line
+# program that prints the ASCII alphabet, in lowercase without  new line
 
-for l in range(97, 123):
-    print('{}'.format(chr(l)), end="")
+for letter in range(97, 123):
+    print('{}'.format(chr(letter)), end="")
