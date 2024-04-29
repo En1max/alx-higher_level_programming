e5fa03e491a5eabc2e03c18ae8ef75b35ae6f976
En1max <moumne2255@gamil.com>
2024-04-29 13:55:50 -0700

    arguments addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
+
 num1 = 0
-lenght = len(argv)
-for i in range(1, lenght):
+length = len(argv)
+for i in range(1, length):
     num1 += int(argv[i])
 print('{}'.format(num1))
