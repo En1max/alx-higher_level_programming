f0c591b4ffe85aa838524e565013f1476659367d
En1max <moumne2255@gamil.com>
2024-05-12 13:09:04 -0700

    function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,9 @@
-#!/usr/bin/pythone3
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    """Add two tuples."""
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
-        else:
-            tuple_a = tuple_a[0], 0
-    if len(tuple_b) < 2:
-        if len(tuple_b) == 0:
-            tuple_b = 0, 0
-        else:
-            tuple_b = tuple_b[0], 0
+    a1 = tuple_a + ('0', '0')
+    b1 = tuple_b + ('0', '0')
 
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    sum1 = int(a1[0]) + int(b1[0])
+    sum2 = int(a1[1]) + int(b1[1])
+
+    return sum1, sum2
