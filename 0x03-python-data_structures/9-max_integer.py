908c33c72109276ac16a98a7f0c42454b9582913
En1max <moumne2255@gamil.com>
2024-05-12 13:36:05 -0700

    max integer of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,5 +2,5 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    my_list.sort
-    print("Max: ", my_list[-1])
+    max_value = my_list.sort
+    print("Max: {}".format(max_value))
