9bfdf385ca3b110e0234aa502c3bfbc75e46947a
En1max <moumne2255@gamil.com>
2024-05-12 14:59:28 -0700

    all multiples of 2 in a list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    multiples = []
+    for i in my_list:
+        if i % 2 == 0:
+            multiples.append(true)
+        else:
+            multiples.append(false)
+    return multiples
